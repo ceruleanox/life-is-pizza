ca901ec0db5a993b2aed141fb5c75a2d1990e025
Evelyn Nguyen <enguyen6208@gmail.com>
2022-02-25 22:02:50 -0800

    home page map

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import Map from '../Layout/Map'
 
 import classes from './Home.module.css';
 
@@ -8,6 +9,11 @@ function Home() {
             <div className={classes['content']}>
                 <div className={classes['home-content']}>
                     This is the Home component (src/pages/home)
+                    <div className='mission-statement'>
+                        <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."                        </p>
+                        <Map />
+                    </div>
                 </div>
             </div>
         </>
