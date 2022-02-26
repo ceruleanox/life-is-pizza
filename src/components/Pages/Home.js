9541118c99754b835e997ccb2fe0d2189cf9eab3
catmcclelland <84889884+catmcclelland@users.noreply.github.com>
2022-02-25 22:49:10 -0500

    add Home page info and About page history

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -7,7 +7,8 @@ function Home() {
         <>
             <div className={classes['content']}>
                 <div className={classes['home-content']}>
-                    This is the Home component (src/pages/home)
+                <p>It’s no secret that the pandemic has taken a toll on the restaurant industry. As of late 2021, nearly 19% of Chicago restaurants had closed since the start of the pandemic.
+Life is Pizza aims to alleviate some of the financial stressors Chicago pizza restaurants are facing by providing them a platform to share their story & sell program-exclusive pizzas. If you are interested in supporting local businesses and eating delicious pizza while you’re at it, look no further!</p>
                 </div>
             </div>
         </>
