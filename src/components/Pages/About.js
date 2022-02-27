586c27eb5c8ec58e3cb221311ee1f70beb4f1c4a
mariessad <74148632+mariessad@users.noreply.github.com>
2022-02-26 21:02:39 -0500

    Home page css, added images, still needs CTA button. About page CSS, does not include map yet

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,20 +1,48 @@
-import { React } from 'react';
+import { React } from "react";
 
-import classes from './About.module.css';
+import classes from "./About.module.css";
+import logo from "../../assets/Chicago-pizza.png";
+import img from "../../assets/pizza-pic.png";
 
 function About() {
-    return (
-        <>
-            <div className={classes['content']}>
-                <div className={classes['about-content']}>
-                <p>Chicago has quite a prolific history when it comes to pizza. Originally brought to Chicago by Neapolitan immigrants, neapolitan-style pizza was quickly upended by a new style of pizza: saucy, cheesy, crunchy deep-dish pizza. </p>
-	<p>There’s a bit of uncertainty regarding who is to thank for inventing deep-dish, but there are several known contenders. Ike Sewell and Ric Riccardo opened the first Pizzeria Uno (now known as Uno Chicago Grill), serving deep-dish pizza. However, a Pizzeria Uno employee named Rudy Malnati claimed that he was the original creator of the recipe. He later opened his own Pizzeria along with his son: Lou Malnati’s Pizzeria. Rudy Malnati’s other son, Rudy Jr, opened his own pizza joint in the early 1990s - Pizano’s. These businesses continue to serve the Chicago area today. </p>
-	<p>Deep-dish isn’t the only pizza deviation to spawn from Chicago, however. In the 1970’s, Nancy’s and Giodarno’s created the stuffed pizza - similar to deep-dish, with an additional layer of dough on top. In addition, Chicago has its own style of pan pizza and thin-crust pizza. </p>
-                </div>
-            </div>
-        </>
-
-    );
+  return (
+    <>
+      <div className={classes["content"]}>
+        <img
+          className={classes["pizza-pic"]}
+          src={img}
+          alt="photo of deep dish pizza"
+        />
+        <div className={classes["about-content"]}>
+          <img className={classes["chicago-pizza"]} src={logo} alt="Logo art" />
+          <p>
+            Chicago has quite a prolific history when it comes to pizza.
+            Originally brought to Chicago by Neapolitan immigrants,
+            neapolitan-style pizza was quickly upended by a new style of pizza:
+            saucy, cheesy, crunchy deep-dish pizza.{" "}
+          </p>
+          <p>
+            There’s a bit of uncertainty regarding who is to thank for inventing
+            deep-dish, but there are several known contenders. Ike Sewell and
+            Ric Riccardo opened the first Pizzeria Uno (now known as Uno Chicago
+            Grill), serving deep-dish pizza. However, a Pizzeria Uno employee
+            named Rudy Malnati claimed that he was the original creator of the
+            recipe. He later opened his own Pizzeria along with his son: Lou
+            Malnati’s Pizzeria. Rudy Malnati’s other son, Rudy Jr, opened his
+            own pizza joint in the early 1990s - Pizano’s. These businesses
+            continue to serve the Chicago area today.{" "}
+          </p>
+          <p>
+            Deep-dish isn’t the only pizza deviation to spawn from Chicago,
+            however. In the 1970’s, Nancy’s and Giodarno’s created the stuffed
+            pizza - similar to deep-dish, with an additional layer of dough on
+            top. In addition, Chicago has its own style of pan pizza and
+            thin-crust pizza.{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,18 +1,40 @@
-import { React } from 'react';
+import { React } from "react";
 
-import classes from './Home.module.css';
+import classes from "./Home.module.css";
+
+import logo from "../../assets/Chicago-pizza.png";
+import img from "../../assets/chicago-skyline-crop.png";
 
 function Home() {
-    return (
-        <>
-            <div className={classes['content']}>
-                <div className={classes['home-content']}>
-                <p>It’s no secret that the pandemic has taken a toll on the restaurant industry. As of late 2021, nearly 19% of Chicago restaurants had closed since the start of the pandemic.
-Life is Pizza aims to alleviate some of the financial stressors Chicago pizza restaurants are facing by providing them a platform to share their story & sell program-exclusive pizzas. If you are interested in supporting local businesses and eating delicious pizza while you’re at it, look no further!</p>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={classes["content"]}>
+        <img
+          className={classes["chicago-skyline"]}
+          src={img}
+          alt="img of chicago skyline"
+        />
+        <div className={classes["home-content"]}>
+          <img className={classes["chicago-pizza"]} src={logo} alt="Logo art" />
+          <p>
+            <span className={classes["life-span"]}>
+              <i>Life is Pizza</i> aims to alleviate some of the financial
+              stressors Chicago pizza restaurants are facing by providing them a
+              platform to share their story & sell program-exclusive pizzas.
+            </span>
+            <span>
+              It’s no secret that the pandemic has taken a toll on the
+              restaurant industry. As of late 2021, nearly 19% of Chicago
+              restaurants had closed since the start of the pandemic. If you are
+              interested in supporting local businesses and eating delicious
+              pizza while you’re at it, look no further!
+            </span>
+          </p>
+          {/* PUT A CALL TO ACTION BUTTON HERE OR SOMETHING */}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
